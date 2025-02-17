f86d4069acb4775d94aec0294b4b32881b41d276
Mik7315 <123501671+Mik7315@users.noreply.github.com>
2025-02-17 21:29:05 +0100

    component client

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ClientComponent } from './client/client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'client',
+    component: ClientComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
