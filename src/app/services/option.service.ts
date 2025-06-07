8d6a02efb47231b874e35e432746bd418d879109
Mik7315 <123501671+Mik7315@users.noreply.github.com>
2025-06-07 16:21:15 +0200

    Get all options Create new option Add options in menu Test ok

---
+++ src/app/services/option.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Option } from "../model/option";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OptionService {
+  private optionPath: string;
+
+  constructor(private http: HttpClient) {
+    this.optionPath = 'http://localhost:8080/api/option';
+  }
+
+  createOption(option: Option): Observable<any> {
+    return this.http.post(this.optionPath, option);
+  }
+
+  getAll(): Observable<Option[]> {
+    return this.http.get<Array<Option>>(this.optionPath);
+  }
+}
